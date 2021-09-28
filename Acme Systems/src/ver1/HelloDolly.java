1bd2f4c7de6daecc9c727dc96ff54ec04f7622cd
Gillcb89 <72765780+Gillcb89@users.noreply.github.com>
2021-09-27 22:34:52 -0400

    Update HelloDolly.java
    added text

--- Acme Systems/src/ver1/HelloDolly.java
+++ Acme Systems/src/ver1/HelloDolly.java
@@ -5,6 +5,7 @@ public class HelloDolly {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HelloDolly");
+		System.out.println("Hellow from github");
 	}
 
 }
