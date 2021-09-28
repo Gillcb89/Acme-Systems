38381c086cd826d1218aa1340975ee7961973c8b
Cabell Bailey Gill <Gillcb89@gmail.com>
2021-09-27 23:55:08 -0400

    added heyNow

--- Acme Systems/src/ver1/HelloDolly.java
+++ Acme Systems/src/ver1/HelloDolly.java
@@ -1,12 +1,19 @@
 package ver1;
 
 public class HelloDolly {
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HelloDolly");
 		System.out.println("Hellow from github");
 		System.out.println("hellow dolly from eclips again and not the first time b/c its broke");
+	
+		
 	}
+	public static void  heyNow() {
+		String Msg = "Hey Now!";
+		System.out.println(Msg);
+		}
 
 }
